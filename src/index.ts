97d9767ee62c490b6726a834315168b3c168b410
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-05 18:37:26 -0300

    codigo se corrigio codigo y agrego boton p/dar funcionalidad

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import "./styles.css";
+rotulo1.innerHTML = "Ingresar monto: ";
+rotulo2.innerHTML = "Ingresar cantidad: ";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+btncalc.addEventListener("click", () => {
+  let monto: number = dato1.value;
+  let cantidad: number = dato2.value;
+  let montoConDescuento: number = 0;
+  let descuento: number = 0;
+  let precioTotal: number = 0;
+  precioTotal = monto * cantidad;
+  if (precioTotal >= 1000) {
+    descuento = (precioTotal * 10) / 100;
+    montoConDescuento = precioTotal - descuento;
+    console.log("Por gastar mas de 1000 tiene un 10% de descuento.");
+    console.log("El monto a pagar es: " + montoConDescuento);
+  } else {
+    console.log("No tiene descuento. El monto a pagar es: " + precioTotal);
+  }
+});
